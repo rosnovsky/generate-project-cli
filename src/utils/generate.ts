d2283a39fbc3b5423c20c4994ea481987e7093f9
Art Rosnovsky <art@rosnovsky.us>
2023-11-25 09:45:45 -0800

    refactor: refactor helper functions

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -1,80 +1,56 @@
-import chalk from "chalk";
 import { ProjectInfo } from "./types.js";
 import fs from "node:fs";
-import { executeCommand, logger } from "./helpers.js";
+import {
+  copyFilesFromTemplate,
+  copyDirsFromTemplate,
+  executeCommand,
+  logger,
+  mkdir,
+  createEmptyFile,
+} from "./helpers.js";
 
 export async function generateProject(projectInfo: ProjectInfo) {
-  const { projectSlug, initializeGit, installDeps } = projectInfo;
+  const { projectSlug, initializeGit, installDeps, projectType } = projectInfo;
 
   const templateDir = process.argv[1].split("/dist")[0] + "/template";
 
   const projectPath = `${process.cwd()}/${projectSlug}`;
 
-  try {
-    fs.mkdirSync(projectPath);
-  } catch (err) {
-    console.error("Failed to create project directory", err);
-    process.exit(1);
-  }
+  const createGenericProject = async () => {
+    mkdir(projectPath);
+    copyFilesFromTemplate(templateDir, projectPath);
+    copyDirsFromTemplate(templateDir, projectPath);
+    mkdir(`${projectPath}/.github/workflows`);
 
-  // Copy template files to project directory, ignore directories
-  fs.readdirSync(templateDir).forEach((file) => {
-    if (!fs.lstatSync(`${templateDir}/${file}`).isDirectory()) {
-      fs.copyFileSync(`${templateDir}/${file}`, `${projectPath}/${file}`);
+    copyFilesFromTemplate(
+      `${templateDir}/.github/workflows`,
+      `${projectPath}/.github/workflows`,
+    );
+
+    createEmptyFile(`${projectPath}/src`, ".keep");
+
+    const pkgJSON = JSON.parse(
+      fs.readFileSync(`${projectPath}/package.json`, "utf8"),
+    );
+
+    pkgJSON.name = projectSlug;
+
+    fs.writeFileSync(
+      `${projectPath}/package.json`,
+      JSON.stringify(pkgJSON, null, 2),
+    );
+
+    if (initializeGit) {
+      executeCommand("git init", projectPath);
     }
-  });
 
-  // create directories that exist in template
-  fs.readdirSync(templateDir).forEach((file) => {
-    if (fs.lstatSync(`${templateDir}/${file}`).isDirectory()) {
-      fs.mkdirSync(`${projectPath}/${file}`);
+    // install dependencies
+    if (installDeps) {
+      executeCommand("pnpm install --no-cache", projectPath);
     }
-  });
+  };
 
-  //create .keep file inside of src directory
-  fs.writeFileSync(`${projectPath}/src/.keep`, "");
-
-  // create a folder `workflows` inside of .github
-  fs.mkdirSync(`${projectPath}/.github/workflows`);
-
-  // copy yaml file to workflows directory
-  fs.copyFileSync(
-    `${templateDir}/.github/workflows/action.yml`,
-    `${projectPath}/.github/workflows/action.yml`,
-  );
-
-  // update package.json
-  const pkgJSON = JSON.parse(
-    fs.readFileSync(`${projectPath}/package.json`, "utf8"),
-  );
-
-  pkgJSON.name = projectSlug;
-
-  fs.writeFileSync(
-    `${projectPath}/package.json`,
-    JSON.stringify(pkgJSON, null, 2),
-  );
-
-  // run git init
-  const { execSync } = await import("child_process");
-  if (initializeGit) {
-    execSync("git init", { cwd: projectPath });
-  }
-
-  // install dependencies
-  if (installDeps) {
-    logger(
-      executeCommand("pnpm install", projectPath),
-      "Installing dependencies",
-      {
-        estimate: 2000,
-      },
-    ).then(() => {
-      console.log(`\n${chalk.green.bold("Done!")}\n`);
-      process.exit(0);
-    });
-  } else {
-    console.log(`\n${chalk.green.bold("Done!")}\n`);
-    process.exit(0);
+  if (projectType === "generic") {
+    await logger(createGenericProject(), "Creating generic project");
   }
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,18 @@
 import { exec } from "child_process";
+import fs from "node:fs";
 import progressEstimator from "progress-estimator";
 
+/**
+ * Progress indicator for long-running commands.
+ */
 export const logger = progressEstimator();
 
+/**
+ * Executes a command in the terminal and returns a promise.
+ * @returns {Promise} - The promise of the command
+ * @param {string} command - The command to execute
+ * @param {string} cwd - The current working directory
+ */
 export function executeCommand(command: string, cwd: string): Promise<any> {
   return new Promise((resolve, reject) => {
     exec(command, { cwd }, (error, stdout, stderr) => {
@@ -14,3 +24,57 @@ export function executeCommand(command: string, cwd: string): Promise<any> {
     });
   });
 }
+
+/**
+ * Creates a directory at the given path
+ * @param {string} projectPath - The path of the project
+ */
+export const mkdir = (projectPath: string) => {
+  try {
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    console.error("Failed to create project directory", err);
+    process.exit(1);
+  }
+};
+
+/**
+ * Copies files from the template directory to the project directory.
+ * @param {string} templateDir - The path of the template directory
+ * @param {string} projectPath - The path of the project directory
+ */
+export const copyFilesFromTemplate = (
+  templateDir: string,
+  projectPath: string,
+) => {
+  try {
+    fs.readdirSync(templateDir).forEach((file) => {
+      if (!fs.lstatSync(`${templateDir}/${file}`).isDirectory()) {
+        fs.copyFileSync(`${templateDir}/${file}`, `${projectPath}/${file}`);
+      }
+    });
+  } catch (err) {
+    console.error("Failed to copy template files", err);
+    process.exit(1);
+  }
+};
+
+/**
+ * Copies directories from the template directory to the project directory.
+ * @param {string} templateDir - The path of the template directory
+ * @param {string} projectPath - The path of the project directory
+ */
+export const copyDirsFromTemplate = (
+  templateDir: string,
+  projectPath: string,
+) => {
+  fs.readdirSync(templateDir).forEach((file) => {
+    if (fs.lstatSync(`${templateDir}/${file}`).isDirectory()) {
+      fs.mkdirSync(`${projectPath}/${file}`);
+    }
+  });
+};
+
+export const createEmptyFile = (projectPath: string, fileName: string) => {
+  fs.writeFileSync(`${projectPath}/${fileName}`, "");
+};
